8a9ec7f2aae9d3fcd217b832ec9340dbc007badd
trive <trive@LAPTOP-JVUAB7TV>
2022-06-29 17:02:22 +0200

    Aggiungere i file di progetto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationPrimoMVCInserimentUjtente.Models;
+
+namespace WebApplicationPrimoMVCInserimentUjtente.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View("prova");
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult UtenteView()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UtenteView(UtenteModel u)
+        {
+            if(ModelState.IsValid)
+                return View("Index");
+            return null;
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplicationPrimoMVCInserimentUjtente.MvcApplication" Language="C#" %>
