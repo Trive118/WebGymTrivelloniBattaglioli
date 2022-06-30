1dfc14c558a6de692b135e21429ad60a1b642661
DaniBatta20 <daniele.battaglioli@studenti.unipr.it>
2022-06-30 17:11:42 +0200

    classe Persona

--- Models/Persona.cs
+++ Models/Persona.cs
@@ -2,5 +2,32 @@
 
 public class Persona
 {
+    int idPersona;
+    string nome;
+    string cognome;
+    string email;
+    string dataNascita;
+    string telefono;
+    char sesso;
+    string ruolo;
 
+    public Persona(int idPersona, string nome, string cognome, string email, string dataNascita, string telefono, char sesso, string ruolo)
+    {
+        this.idPersona = idPersona;
+        this.nome = nome;
+        this.cognome = cognome;
+        this.email = email;
+        this.dataNascita = dataNascita;
+        this.telefono = telefono;
+        this.sesso = sesso;   
+        this.ruolo = ruolo;
+    }
+    public int IdPersona { get => idPersona; set => idPersona = value; }
+    public string Nome { get => nome; set => nome = value; }
+    public string Cognome { get => cognome; set => cognome = value; }
+    public string Email { get => email; set => email = value; }
+    public string DataNascita { get => dataNascita; set => dataNascita = value; }
+    public string Telefono { get => telefono; set => telefono = value; }
+    public char Sesso { get => sesso; set => sesso = value; }
+    public string Ruolo { get => ruolo; set => ruolo = value; }
 }
