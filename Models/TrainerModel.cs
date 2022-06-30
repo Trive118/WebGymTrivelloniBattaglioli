eb22d84f01773f1af990d4f0ca0576dd7fd572d7
DaniBatta20 <daniele.battaglioli@studenti.unipr.it>
2022-06-30 19:32:28 +0200

    Aggiunto TrainerModel

---
+++ Models/TrainerModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebGymTrivelloniBattaglioli.Models
+{
+    public class TrainerModel : Persona
+    {
+
+        public TrainerModel(int idTrainer, string nome, string cognome, string email, DateTime data_nascita, string telefono, definizioneSesso sesso) : base(idTrainer, nome, cognome, email, data_nascita, telefono, sesso)
+        {
+
+        }
+
+    }
+}
