64d34abd863ef74a27646e8972c98b9f530309ce
DaniBatta20 <108366709+DaniBatta20@users.noreply.github.com>
2022-06-30 16:12:35 +0200

    Update QueryStart.sql

--- Documetazione/QueryStart.sql
+++ Documetazione/QueryStart.sql
@@ -15,10 +15,63 @@ CREATE TABLE Contratto
 (
     id int(8) PRIMARY KEY AUTO_INCREMENT,
     descrizione varchar(100) NOT NULL,
-    prezzo double(4,2) NOT NULL,
+    prezzo double(6,2) NOT NULL,
     durata int(2) NOT NULL,
     
     CONSTRAINT ControlloDurata CHECK(durata>0),
     CONSTRAINT ControlloPrezzo CHECK(prezzo>0.00)
 )
+
+CREATE TABLE Iscrizione
+(
+    id int(8) PRIMARY KEY AUTO_INCREMENT,
+    data_inizio timestamp NOT NULL,
+    motivo varchar(100) NOT NULL,
+    idPersona int(6),
+    idContratto int(8),
+    FOREIGN KEY (idPersona) REFERENCES Persona(id),
+    FOREIGN KEY (idContratto) REFERENCES Contratto(id)
+    
+)
+
+CREATE TABLE Scheda
+(
+    id int(6) PRIMARY KEY AUTO_INCREMENT,
+    idTrainer int(6),
+    idCliente int(6),
+    titolo varchar(50) NOT NULL,
+    durata int(2) NOT NULL,
+    
+    CONSTRAINT ControlloDurataScheda CHECK(durata>0),
+    FOREIGN KEY (idTrainer) REFERENCES Persona(id),
+    FOREIGN KEY (idCliente) REFERENCES Persona(id)
+)
+
+
+
+CREATE TABLE Esercizio
+(
+    id int(5) PRIMARY KEY AUTO_INCREMENT,
+    immagine varchar(100) NOT NULL,
+    descrizione varchar(100) NOT NULL
+    
+)
+
+CREATE TABLE Associazione
+(
+    idScheda int(6),
+    idEsercizio int(5),
+    
+    numeroRipetizioni int(2) NOT NULL,
+    recupero time NOT NULL,
+    commento varchar(50),
+    
+    FOREIGN KEY (idScheda) REFERENCES Scheda(id),
+    FOREIGN KEY (idEsercizio) REFERENCES Esercizio(id),
+    PRIMARY KEY(idScheda, idEsercizio)
+)
+
+
+
+
 
