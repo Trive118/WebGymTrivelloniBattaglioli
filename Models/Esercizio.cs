fa4b3c7c3b934180fbedabaeaf21312ee9486ed9
Francesco TRIVELLONI <francesco.trivelloni1@studenti.unipr.it>
2022-06-30 19:36:58 +0200

    esercizio

---
+++ Models/Esercizio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebGymTrivelloniBattaglioli.Models
+{
+    public class Esercizio
+    {
+        int id;
+        string descrizione;
+        string immagine;
+
+        public Esercizio(int id, string descrizione, string immagine)
+        {
+            this.id = id;
+            this.descrizione = descrizione;
+            this.immagine = immagine;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Descrizione { get => descrizione; set => descrizione = value; }
+        public string Immagine { get => immagine; set => immagine = value; }
+    }
+}
