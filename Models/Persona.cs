e8624c84c2656edf43cce84409605cd8b69a842f
Francesco TRIVELLONI <francesco.trivelloni1@studenti.unipr.it>
2022-06-30 17:24:28 +0200

    upload

--- Models/Persona.cs
+++ Models/Persona.cs
@@ -1,33 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 
-public class Persona
+
+namespace WebGymTrivelloniBattaglioli.Models
 {
-    int idPersona;
-    string nome;
-    string cognome;
-    string email;
-    string dataNascita;
-    string telefono;
-    char sesso;
-    string ruolo;
-
-    public Persona(int idPersona, string nome, string cognome, string email, string dataNascita, string telefono, char sesso, string ruolo)
+    public class Persona
     {
-        this.idPersona = idPersona;
-        this.nome = nome;
-        this.cognome = cognome;
-        this.email = email;
-        this.dataNascita = dataNascita;
-        this.telefono = telefono;
-        this.sesso = sesso;   
-        this.ruolo = ruolo;
+        int idPersona;
+        string nome;
+        string cognome;
+        string email;
+        string dataNascita;
+        string telefono;
+        char sesso;
+        string ruolo;
+
+        public Persona(int idPersona, string nome, string cognome, string email, string dataNascita, string telefono, char sesso, string ruolo)
+        {
+            this.idPersona = idPersona;
+            this.nome = nome;
+            this.cognome = cognome;
+            this.email = email;
+            this.dataNascita = dataNascita;
+            this.telefono = telefono;
+            this.sesso = sesso;
+            this.ruolo = ruolo;
+        }
+        public int IdPersona { get => idPersona; set => idPersona = value; }
+        public string Nome { get => nome; set => nome = value; }
+        public string Cognome { get => cognome; set => cognome = value; }
+        public string Email { get => email; set => email = value; }
+        public string DataNascita { get => dataNascita; set => dataNascita = value; }
+        public string Telefono { get => telefono; set => telefono = value; }
+        public char Sesso { get => sesso; set => sesso = value; }
+        public string Ruolo { get => ruolo; set => ruolo = value; }
     }
-    public int IdPersona { get => idPersona; set => idPersona = value; }
-    public string Nome { get => nome; set => nome = value; }
-    public string Cognome { get => cognome; set => cognome = value; }
-    public string Email { get => email; set => email = value; }
-    public string DataNascita { get => dataNascita; set => dataNascita = value; }
-    public string Telefono { get => telefono; set => telefono = value; }
-    public char Sesso { get => sesso; set => sesso = value; }
-    public string Ruolo { get => ruolo; set => ruolo = value; }
 }
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Le informazioni generali relative a un assembly sono controllate dal seguente
 // set di attributi. Modificare i valori di questi attributi per modificare le informazioni
 // associate a un assembly.
-[assembly: AssemblyTitle("WebApplicationPrimoMVCInserimentUjtente")]
+[assembly: AssemblyTitle("WebGymTrivelloniBattaglioli")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WebApplicationPrimoMVCInserimentUjtente")]
+[assembly: AssemblyProduct("WebGymTrivelloniBattaglioli")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
